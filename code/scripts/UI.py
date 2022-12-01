973bfab2e2e2c809a1d485b867efa98c59f09451
Guizmus <bertier.antoine@gmail.com>
2022-12-01 23:55:49 +0100

    Instal UI revision

--- code/scripts/UI.py
+++ code/scripts/UI.py
@@ -1,8 +1,8 @@
 from os import path, startfile
-from time import sleep
 import subprocess
 import PySimpleGUI as sg
-sg.theme('Topanga')
+
+sg.theme('Dark Black')
 
 scripts = [
     ["Training UI : Diffusers Comparison","code\\scripts\\installDiffusers.bat","code\\resources\\diffusers\\"],
@@ -12,7 +12,7 @@ scripts = [
     ["Diffusers : 2.0 diffusers (512)","code\\scripts\\installDiffusers2-0-512.bat","data\\diffusers\\SD-2.0-512\\"],
     ["Diffusers : 2.0 diffusers (768)","code\\scripts\\installDiffusers2-0-768.bat","data\\diffusers\\SD-2.0-768\\"],
     ["Diffusers : 2.0 diffusers (4xUp)","code\\scripts\\installDiffusers2-0-4xUpscaler.bat","data\\diffusers\\SD-2.0-4xUpscaler\\"],
-    ["Example run dataset","code\\scripts\\installExample.bat","data\\datasets\\example\\"]
+    ["Example run dataset","code\\scripts\\installExample.bat","data\\datasets\\example\\"],
 ]
 
 c1=[]
@@ -21,76 +21,68 @@ i = 0
 for script in scripts:
     isInstalled = path.isdir(script[2])
     c1.append([
-        sg.Checkbox(script[0],key='-INSTALL-'+str(i)+'-',default=isInstalled,disabled=isInstalled)
+        sg.Checkbox(script[0],key='INSTALL-'+str(i),default=isInstalled,disabled=isInstalled)
     ])
     c2.append([
-        sg.Button("Open folder",key='-OPEN-'+str(i)+'-',disabled=not isInstalled)
+        sg.Button("Open folder",key='OPEN-'+str(i),disabled=not isInstalled)
     ])
     i = i + 1
 layout=[[sg.Col(c1),sg.Col(c2)]]
 layout.append([
-    sg.Button("Install checked resources",key='-START-INSTALL-')
+    sg.Button("Install checked resources",key='START')
 ]) 
 layout.append([
-    [sg.StatusBar("",key='-STATUSBAR-',size=(40,1))]
+    [sg.StatusBar("",key='STATUSBAR',size=(40,1))]
 ])
 
 window = sg.Window('DreamboothSimpleUI Installer', layout)
 
+runningInstall=False
+currentProcess=None
+currentScript=0
+tried_jobs = []
 while True:
-    event, values = window.read()
-    if event != None:
-        subevent = event.split('-')
-        
-        if subevent[1] == "OPEN":
-            script = scripts[int(subevent[2])]
-            startfile(script[2])
-                
-    if event == "-START-INSTALL-":
-        i=0
-        confirmtext="Are you sure you want to install :\n"
-        installlist=[]
-        for script in scripts:
-            isInstalled = path.isdir(script[2])
-            isChecked = window['-INSTALL-'+str(i)+'-'].get()
-            if (not isInstalled) and isChecked:
-                confirmtext = confirmtext+"- "+script[0]+"\n"
-                installlist.append(i)
-            i=i+1
-        if len(installlist) > 0:
-            if (sg.popup_yes_no(confirmtext,title="Intall confim")) == "Yes":
-                window['-START-INSTALL-'](disabled=True)
-                report = ""
-                for i in installlist:
-                    script=scripts[i]
-                    print("Executing script : "+script[1])
-                    status = "Installing "+script[0]
-                    currentInstall = subprocess.Popen([script[1]])
-                    j=""
-                    while currentInstall.poll() == None:
-                    # while True:
-                        j=j+"."
-                        if len(j)>3:
-                            j="."
-                        window['-STATUSBAR-'](status+j)
-                        window.refresh()
-                        sleep(1)
-                    if path.isdir(script[2]):
-                        window['-INSTALL-'+str(i)+'-'](disabled=True,value=True)
-                        window['-OPEN-'+str(i)+'-'](disabled=False)
-                        window['-STATUSBAR-']("Installed "+script[0])
-                        report = report + script[0]+":Success "
-                    else:
-                        window['-STATUSBAR-']("Failed installation : "+script[0])
-                        report = report + script[0]+":Fail "
-                    window.refresh()
-                window['-START-INSTALL-'](disabled=False)
-                window['-STATUSBAR-'](report)
-                window.refresh()
-        else:
-            sg.popup("Nothing to install.\nCheck the boxes in front of the items you want to install first.",title="Nothing to install")
-            
-    if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
+    event, values = window.read(timeout=500 if runningInstall else None)
+    if (event == None) or (event == sg.WIN_CLOSED):
         break
+    
+    subevent = event.split('-')
+    if subevent[0] == "OPEN":
+        script = scripts[int(subevent[1])]
+        startfile(script[2])
+    elif subevent[0] == "START":
+        runningInstall=True
+        window['START'](disabled=True)
+        
+    if runningInstall:
+        if not (currentProcess is None): #if a process is running
+            if not (currentProcess.poll() is None):
+                tried_jobs.append(currentScript)
+                if path.isdir(scripts[currentScript][2]):
+                    window['INSTALL-'+str(currentScript)](disabled=True,value=True)
+                    window['OPEN-'+str(currentScript)](disabled=False)
+                else:
+                    window['INSTALL-'+str(currentScript)](disabled=False,value=False)
+                    window['STATUSBAR']("Install failed : "+scripts[currentScript][0])
+                currentProcess = None
+                currentScript = 0
+        if currentProcess is None: #if no process is running
+            next_script = None
+            for i in range(len(scripts)):
+                script = scripts[i]
+                if (window['INSTALL-'+str(i)].get()) and not i in tried_jobs:
+                    isInstalled = path.isdir(script[2])
+                    if not isInstalled:
+                        next_script = i
+                        break
+            if next_script is None:#nothing to install
+                runningInstall=False
+                tried_jobs = []
+                window['START'](disabled=False)
+            else:
+                currentScript = next_script
+                window['INSTALL-'+str(currentScript)](disabled=True)
+                window['STATUSBAR']("Installing  "+scripts[currentScript][0])
+                currentProcess = subprocess.Popen([scripts[currentScript][1]])
 
 window.close()
